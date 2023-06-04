fadda0dba266cb56a791c95125e7ec9af8390af5
Capmu <p.kanawat.capmu@gmail.com>
2023-06-04 13:30:25 +0700

    add getting orders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { OrderDialogComponent } from './order-dialog/order-dialog.component';
 import { FormGroup } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { BackendService } from './services/backend.service';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +12,30 @@ import { FormGroup } from '@angular/forms';
 })
 export class AppComponent {
   title = 'oms-2023';
+  dataSource!: MatTableDataSource<any>;
 
-  constructor(private dialog: MatDialog) { }
+  constructor(
+    private dialog: MatDialog,
+    private backendService: BackendService) { }
+
+  ngOnInit(): void {
+    this.getAllOrders()
+  }
 
   openOrderDialog() {
     this.dialog.open(OrderDialogComponent, { width: '30%' });
+  }
+
+  getAllOrders() {
+    this.backendService.getOrder()
+      .subscribe({
+        next: (response) => {
+          this.dataSource = new MatTableDataSource(response)
+          console.log(this.dataSource.data)  //to check the fetched data
+        },
+        error: () => {
+          alert('Error while getting orders')
+        }
+      })
   }
 }
